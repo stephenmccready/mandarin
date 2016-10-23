d55fb707778a6c53088f00e60bec3f91e19d1a27
Stephen McCready <stephenmccready@gmail.com>
2016-10-23 11:46:22 +0800

    Create numbers.php

---
+++ numbers.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" content=""/>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>&#128290; numbers 数</title>
+	<link type="text/css" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<!--[if IE]>
+	<script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<link rel="stylesheet" href="css/chinese.css">
+</head>
+<body>
+<audio controls="" id="ling2"><source src="audio/ling2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="yi1"><source src="audio/yi1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="er4"><source src="audio/er4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="san1"><source src="audio/san1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="si4"><source src="audio/si4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="wu3"><source src="audio/wu3.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="liu4"><source src="audio/liu4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="qi1"><source src="audio/qi1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="ba1"><source src="audio/ba1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="jiu3"><source src="audio/jiu3.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="shi2"><source src="audio/shi2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="bai3"><source src="audio/bai3.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="qian1"><source src="audio/qian1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="wan4"><source src="audio/wan4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="yi2"><source src="audio/yi2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="yi4"><source src="audio/yi4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<?php include 'navbar.html'; ?>
+	<div class="container-fluid">
+		<div class="box">
+			<div>0</div><div class="character">零</div><div class="btn-group btn-group-lg">
+				<button type="button" class="btn btn-huge btn-warning" onclick="play('ling2');">líng</button>
+			</div>
+		</div>
+		<div class="box">
+			<div>1</div><div class="character">一</div><div class="btn-group btn-group-lg">
+				<button type="button" class="btn btn-huge btn-danger" onclick="play('yi1');">yī</button>
+			</div>
+		</div>
+		<div class="box">
+			<div>2</div><div class="character">二</div><div class="btn-group btn-group-lg">
+				<button type="button" class="btn btn-huge btn-primary" onclick="play('er4');">èr</button>
+			</div>
+		</div>
+		<div class="box">
+			<div>3</div><div class="character">三</div><div class="btn-group btn-group-lg">
+				<button type="button" class="btn btn-huge btn-danger" onclick="play('san1');">sān</button>
+			</div>
+		</div>
+		<div class="box">
+			<div>4</div><div class="character">四</div><div class="btn-group btn-group-lg">
+				<button type="button" class="btn btn-huge btn-primary" onclick="play('si4');">sì</button>
+			</div>
+		</div>
+		<div class="box">
+			<div>5</div><div class="character">五</div><div class="btn-group btn-group-lg">
+				<button type="button" class="btn btn-huge btn-success" onclick="play('wu3');">wǔ</button>
+			</div>
+			</div>
+		<div class="box">
+			<div>6</div><div class="character">六</div><div class="btn-group btn-group-lg">
+				<button type="button" class="btn btn-huge btn-primary" onclick="play('liu4');">liù</button>
+			</div>
+		</div>
+		<div class="box">
+			<div>7</div><div class="character">七</div><div class="btn-group btn-group-lg">
+				<button type="button" class="btn btn-huge btn-danger" onclick="play('qi1');">qī</button>
+			</div>
+		</div>
+		<div class="box">
+			<div>8</div><div class="character">八</div><div class="btn-group btn-group-lg">
+				<button type="button" class="btn btn-huge btn-danger" onclick="play('ba1');">bā</button>
+			</div>
+		</div>
+		<div class="box">
+			<div>9</div><div class="character">九</div><div class="btn-group btn-group-lg">
+				<button type="button" class="btn btn-huge btn-success" onclick="play('jiu3');">jiǔ</button>
+			</div>
+		</div>
+		<div class="box">
+			<div>10</div><div class="character">十</div><div class="btn-group btn-group-lg">
+				<button type="button" class="btn btn-huge btn-warning" onclick="play('shi2');">shí</button>
+			</div>
+		</div>
+		<div class="box">
+			<div>11</div><div class="character">一十</div><div class="btn-group btn-group-lg">
+				<button type="button" class="btn btn-huge btn-danger" onclick="play('yi1');">yī</button>
+				<button type="button" class="btn btn-huge btn-warning" onclick="play('shi2');">shí</button>
+			</div>
+		</div>
+		<div class="box">
+			<div>21</div><div class="character">二十一</div><div class="btn-group btn-group-lg">
+				<button type="button" class="btn btn-huge btn-primary" onclick="play('er4');">èr</button>
+				<button type="button" class="btn btn-huge btn-warning" onclick="play('shi2');">shí</button>
+				<button type="button" class="btn btn-huge btn-danger" onclick="play('yi1');">yī</button>
+			</div>
+		</div>
+		<div class="box">
+			<div>100</div><div class="character">百</div><div class="btn-group btn-group-lg">
+				<button type="button" class="btn btn-huge btn-success" onclick="play('bai3');">bǎi</button>
+			</div>
+		</div>
+		<div class="box">
+			<div>1000</div><div class="character">千</div><div class="btn-group btn-group-lg">
+				<button type="button" class="btn btn-huge btn-danger" onclick="play('qian1');">qiān</button>
+			</div>
+		</div>
+		<div class="box">
+			<div>1,0000<br />ten thousand</div><div class="character">万</div><div class="btn-group btn-group-lg">
+				<button type="button" class="btn btn-huge btn-warning" onclick="play('yi2');">yí</button>
+				<button type="button" class="btn btn-huge btn-primary" onclick="play('wan4');">wàn</button>
+			</div>
+		</div>
+		<div class="box">
+			<div>100,0000<br />million</div><div class="character">百万</div><div class="btn-group btn-group-lg">
+				<button type="button" class="btn btn-huge btn-primary" onclick="play('yi4');">yì</button>
+				<button type="button" class="btn btn-huge btn-success" onclick="play('bai3');">bǎi</button>
+				<button type="button" class="btn btn-huge btn-primary" onclick="play('wan4');">wàn</button>
+			</div>
+		</div>
+		<div class="box">
+			<div>1,0000,0000<br />hundred million</div><div class="character">亿</div><div class="btn-group btn-group-lg">
+				<button type="button" class="btn btn-huge btn-warning" onclick="play('yi2');">yí</button>
+				<button type="button" class="btn btn-huge btn-primary" onclick="play('yi4');">yì</button>
+			</div>
+		</div>
+	</div>
+	<script src="js/chinese.js"></script>
+<script>$("#breadcrumb").html("&#9986;&nbsp;basics&nbsp;基本 > &nbsp;&#128290;&nbsp;numbers&nbsp;数 >");</script>
+</body>
+</html>
