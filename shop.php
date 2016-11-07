5d718de42b5e6fcd6a782b68ac634746d2c7140a
Stephen McCready <stephenmccready@gmail.com>
2016-11-07 21:01:26 +0800

    Create shop.php

---
+++ shop.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" content=""/>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>&#128179; shopping 店</title>
+	<link type="text/css" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<!--[if IE]>
+	<script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<link rel="stylesheet" href="css/chinese.css">
+</head>
+<body>
+<audio controls="" id="yuan2"><source src="audio/yuan2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="duo1"><source src="audio/duo1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="shao3"><source src="audio/shao3.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="ni3"><source src="audio/ni3.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="you3"><source src="audio/you3.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="kan4"><source src="audio/kan4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="xiao3"><source src="audio/xiao3.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="zhong1"><source src="audio/zhong1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="da4"><source src="audio/da4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="jia1"><source src="audio/jia1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="hao4"><source src="audio/hao4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="ling3"><source src="audio/ling3.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="dai4"><source src="audio/dai4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="xi1"><source src="audio/xi1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="fu2"><source src="audio/fu2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="wai4"><source src="audio/wai4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="tao4"><source src="audio/tao4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="jai2"><source src="audio/jai2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="ke4"><source src="audio/ke4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="qun2"><source src="audio/qun2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="zi1"><source src="audio/zi1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="shou3"><source src="audio/shou3.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="tao4"><source src="audio/tao4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<?php include 'navbar.html'; ?>
+<div class="container-fluid">
+	<div class="box">
+		<div>yuán</div><div class="character">元</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('yuan2');">yuán</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>how much?</div><div class="character">若干</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('duo1');">duō</button>
+			<button type="button" class="btn btn-huge btn-success" onclick="play('shao3');">shǎo</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>do you have?</div><div class="character">你有</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-success" onclick="play('ni3');">nǐ</button>
+			<button type="button" class="btn btn-huge btn-success" onclick="play('you3');">yǒu</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>just looking</div><div class="character">看看</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-primary" onclick="play('kan4');">kàn</button>
+			<button type="button" class="btn btn-huge btn-primary" onclick="play('kan4');">kàn</button>
+		</div>
+	</div>
+	<div class="clearfix"></div>
+	<div class="box">
+		<div>small</div><div class="character">小</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-success" onclick="play('xiao3');">xiǎo</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>medium</div><div class="character">中</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('zhong1');">zhōng</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>large</div><div class="character">大</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-primary" onclick="play('da4');">dà</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>extra large</div><div class="character">加大号</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('jia1');">jiā</button>
+			<button type="button" class="btn btn-huge btn-primary" onclick="play('da4');">dà</button>
+			<button type="button" class="btn btn-huge btn-primary" onclick="play('hao4');">hào</button>
+		</div>
+	</div>
+	<div class="clearfix"></div>
+	<div class="box">
+		<div>tie</div><div class="character">领带</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-success" onclick="play('ling3');">lǐng</button>
+			<button type="button" class="btn btn-huge btn-primary" onclick="play('dai4');">dài</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>suit</div><div class="character">西服</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('xi1');">xī</button>
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('fu2');">fú</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>coat</div><div class="character">外套</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-primary" onclick="play('wai4');">wài</button>
+			<button type="button" class="btn btn-huge btn-primary" onclick="play('tao4');">tào</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>jacket</div><div class="character">夹克</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('jia2');">jiá</button>
+			<button type="button" class="btn btn-huge btn-primary" onclick="play('ke4');">kè</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>skirt</div><div class="character">裙子</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('qun2');">qún</button>
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('zi1');">zi</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>gloves</div><div class="character">手套</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-success" onclick="play('shou3');">shǒu</button>
+			<button type="button" class="btn btn-huge btn-primary" onclick="play('tao4');">tào</button>
+		</div>
+	</div>
+</div>
+<script src="js/chinese.js"></script>
+<script>$("#breadcrumb").html("&#9986;&nbsp;basics&nbsp;基本 > &#128179;&nbsp;shopping&nbsp;店 >");</script>
+</body>
+</html>
