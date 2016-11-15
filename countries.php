6657d5ac4f7a3cd3833375141aebe3884438bca3
Stephen McCready <stephenmccready@gmail.com>
2016-11-15 17:41:48 +0800

    Create countries.php

---
+++ countries.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" content=""/>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>&#127759; countries 地方</title>
+	<link type="text/css" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<!--[if IE]>
+	<script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<link rel="stylesheet" href="css/chinese.css">
+</head>
+<body>
+<audio controls="" id="zhong1"><source src="audio/zhong1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="guo2"><source src="audio/guo2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="mei3"><source src="audio/mei3.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="jia1"><source src="audio/jia1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="na2"><source src="audio/na2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="da4"><source src="audio/da4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="su1"><source src="audio/su1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="ge2"><source src="audio/ge2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="lan2"><source src="audio/lan2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="ying1"><source src="audio/ying1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="ri4"><source src="audio/ri4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="ben3"><source src="audio/ben3.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="xin1"><source src="audio/xin1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="po1"><source src="audio/po1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="tai2"><source src="audio/tai2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="wan1"><source src="audio/wan1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="han2"><source src="audio/han2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="bei3"><source src="audio/bei3.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="chao2"><source src="audio/chao2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="xian3"><source src="audio/xian3.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<?php include 'navbar.html'; ?>
+<div class="container-fluid">
+	<div class="box">
+		<div>China &#127464;&#127475;<br />middle + country</div><div class="character">中国</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('zhong1');">Zhōng</button>
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('guo2');">guó</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>United States &#127482;&#127480;<br />beautiful + country</div><div class="character">美国</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-success" onclick="play('mei3');">Měi</button>
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('guo2');">guó</button>
+		</div>
+	</div>
+	<div class="clearfix"></div>
+	<div class="box">
+		<div>Canada &#127464;&#127462;</div><div class="character">加拿大</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('jia1');">Jiā</button>
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('na2');">ná</button>
+			<button type="button" class="btn btn-huge btn-primary" onclick="play('da4');">dà</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>Scotland &#127485;&#127480;</div><div class="character">苏格兰</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('su1');">Sū</button>
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('ge2');">gé</button>
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('lan2');">lán</button>
+		</div>
+	</div>
+	<div class="clearfix"></div>
+	<div class="box">
+		<div>Japan &#127471;&#127477;<br />sun + origin</div><div class="character">日本</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-primary" onclick="play('ri4');">Rì</button>
+			<button type="button" class="btn btn-huge btn-success" onclick="play('ben3');">běn</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>South Korea &#127472;&#127479;<br />Republic</div><div class="character">韩国</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('han2');">Hán</button>
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('guo2');">guó</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>North Korea &#127472;&#127477;<br />Democratic People's Republic</div><div class="character">北朝鲜</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-success" onclick="play('bei3');">Běi</button>
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('chao2');">cháo</button>
+			<button type="button" class="btn btn-huge btn-success" onclick="play('xian3');">xiǎn</button>
+		</div>
+	</div>
+	<div class="clearfix"></div>
+	<div class="box">
+		<div>Singapore &#127480;&#127468;</div><div class="character">新加坡</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('xin1');">Xīn</button>
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('jia1');">jiā</button>
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('po1');">pō</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>Taiwan &#127481;&#127484;</div><div class="character">台湾</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('tai2');">Tái</button>
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('wan1');">wān</button>
+		</div>
+	</div>
+	<div class="clearfix"></div>
+	<div class="box">
+		<div>England &#127485;&#127466;<br />brave + country</div><div class="character">英国</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('ying1');">Yīng</button>
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('guo2');">guó</button>
+		</div>
+	</div>
+</div>
+<script src="js/chinese.js"></script>
+<script>$("#breadcrumb").html("&#128507;&nbsp;geography&nbsp;地理 > &#127759;&nbsp;countries&nbsp;地方 >");</script>
+</body>
+</html>
