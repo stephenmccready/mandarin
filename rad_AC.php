0ae1cccbd4784866d1a71575fde024665f83d2a8
Stephen McCready <stephenmccready@gmail.com>
2016-11-15 21:34:44 +0800

    Create rad_AC.php

---
+++ rad_AC.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head><meta charset="utf-8" content=""/><meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>&#174; radicals A - C</title><link type="text/css" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"><script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script><script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script><!--[if IE]><script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]--><link rel="stylesheet" href="css/chinese.css">
+</head>
+<body>
+<audio controls="" id="you4"><source src="audio/you4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="er2"><source src="audio/er2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="zhi4"><source src="audio/zhi4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="shi3"><source src="audio/shi3.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="jin1"><source src="audio/jin1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="zhu2"><source src="audio/zhu2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="dou4"><source src="audio/dou4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="da4"><source src="audio/da4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="niao3"><source src="audio/niao3.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="xin1"><source src="audio/xin1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="hei1"><source src="audio/hei1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="xue3"><source src="audio/xue3.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="qing1"><source src="audio/qing1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="zhou1"><source src="audio/zhou1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="shen1"><source src="audio/shen1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="pi3"><source src="audio/pi3.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="gu3"><source src="audio/gu3.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="gong1"><source src="audio/gong1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="zhi1"><source src="audio/zhi1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="shan1"><source src="audio/shan1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="yu4"><source src="audio/yu4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="che1"><source src="audio/che1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="li4"><source src="audio/li4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="xue4"><source src="audio/xue4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="zi3"><source src="audio/zi3.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="yi4"><source src="audio/yi4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="shi4"><source src="audio/shi4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="zhao3"><source src="audio/zhao3.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="han4"><source src="audio/han4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="yi1"><source src="audio/yi1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="se4"><source src="audio/se4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="bi3"><source src="audio/bi3.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="shi1"><source src="audio/shi1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="niu2"><source src="audio/niu2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<?php include 'navbar.html'; ?>
+<div class="container-fluid">
+	<div class="directions">
+	<table class="radical">
+		<tr><th colspan="4"> </th></tr>
+		<tr><th colspan="4">A</th></tr>
+		<tr><td>又</td><td></td><td><button type="button" class="btn btn-huge btn-primary" onclick="play('you4');">you4</button></td><td>again</td></tr>
+		<tr><td>而</td><td></td><td><button type="button" class="btn btn-huge btn-warning" onclick="play('er2');">er2</button></td><td>and</td></tr>
+		<tr><td>至</td><td></td><td><button type="button" class="btn btn-huge btn-primary" onclick="play('zhi4');">zhi4</button></td><td>arrive</td></tr>
+		<tr><td>矢</td><td></td><td><button type="button" class="btn btn-huge btn-success" onclick="play('shi3');">shi3</button></td><td>arrow</td></tr>
+		<tr><td>斤</td><td></td><td><button type="button" class="btn btn-huge btn-danger" onclick="play('jin1');">jin1</button></td><td>axe</td></tr>
+		<tr><th colspan="4"> </th></tr>
+		<tr><th colspan="4">B</th></tr>
+		<tr><td>豸</td><td></td><td><button type="button" class="btn btn-huge btn-primary" onclick="play('zhi4');">zhi4</button></td><td>badger</td></tr>
+		<tr><td>竹</td><td>⺮</td><td><button type="button" class="btn btn-huge btn-warning" onclick="play('zhu2');">zhu2</button></td><td>bamboo</td></tr>
+		<tr><td>豆</td><td></td><td><button type="button" class="btn btn-huge btn-primary" onclick="play('dou4');">dou4</button></td><td>bean</td></tr>
+		<tr><td>大</td><td></td><td><button type="button" class="btn btn-huge btn-primary" onclick="play('da4');">da4</button></td><td>big</td></tr>
+		<tr><td>鳥</td><td>鸟</td><td><button type="button" class="btn btn-huge btn-success" onclick="play('niao3');">niao3</button></td><td>bird</td></tr>
+		<tr><td>辛</td><td></td><td><button type="button" class="btn btn-huge btn-danger" onclick="play('xin1');">xin1</button></td><td>bitter</td></tr>
+		<tr><td>黑</td><td></td><td><button type="button" class="btn btn-huge btn-danger" onclick="play('hei1');">hei1</button></td><td>black</td></tr>
+		<tr><td>血</td><td></td><td><button type="button" class="btn btn-huge btn-success" onclick="play('xue3');">xue3</button></td><td>blood</td></tr>
+		<tr><td>青</td><td></td><td><button type="button" class="btn btn-huge btn-danger" onclick="play('qing1');">qing1</button></td><td>blue</td></tr>
+		<tr><td>舟</td><td></td><td><button type="button" class="btn btn-huge btn-danger" onclick="play('zhou1');">zhou1</button></td><td>boat</td></tr>
+		<tr><td>身</td><td></td><td><button type="button" class="btn btn-huge btn-danger" onclick="play('shen1');">shen1</button></td><td>body</td></tr>
+		<tr><td>疋</td><td></td><td><button type="button" class="btn btn-huge btn-success" onclick="play('pi3');">pi3</button></td><td>bolt of cloth</td></tr>
+		<tr><td>骨</td><td></td><td><button type="button" class="btn btn-huge btn-success" onclick="play('gu3');">gu3</button></td><td>bone</td></tr>
+		<tr><td>弓</td><td></td><td><button type="button" class="btn btn-huge btn-danger" onclick="play('gong1');">gong1</button></td><td>bow</td></tr>
+		<tr><td>支</td><td></td><td><button type="button" class="btn btn-huge btn-danger" onclick="play('zhi1');">zhi1</button></td><td>branch</td></tr>
+		<tr><td>彡</td><td></td><td><button type="button" class="btn btn-huge btn-danger" onclick="play('shan1');">shan1</button></td><td>bristle</td></tr>
+		<tr><td>聿</td><td></td><td><button type="button" class="btn btn-huge btn-primary" onclick="play('yu4');">yu4</button></td><td>brush</td></tr>
+		<tr><th colspan="4"> </th></tr>
+		<tr><th colspan="4">C</th></tr>
+		<tr><td>車</td><td>车</td><td><button type="button" class="btn btn-huge btn-danger" onclick="play('che1');">che1</button></td><td>cart</td></tr>
+		<tr><td>鬲</td><td></td><td><button type="button" class="btn btn-huge btn-primary" onclick="play('li4');">li4</button></td><td>caul­dron</td></tr>
+		<tr><td>穴</td><td></td><td><button type="button" class="btn btn-huge btn-primary" onclick="play('xue4');">xue4</button></td><td>cave</td></tr>
+		<tr><td>子</td><td></td><td><button type="button" class="btn btn-huge btn-success" onclick="play('zi3');">zi3</button></td><td>child</td></tr>
+		<tr><td>邑</td><td>阝</td><td><button type="button" class="btn btn-huge btn-primary" onclick="play('yi4');">yi4</button></td><td>city</td></tr>
+		<tr><td>氏</td><td></td><td><button type="button" class="btn btn-huge btn-primary" onclick="play('shi4');">shi4</button></td><td>clan</td></tr>
+		<tr><td>爪</td><td>爫</td><td><button type="button" class="btn btn-huge btn-success" onclick="play('zhao3');">zhao3</button></td><td>claw</td></tr>
+		<tr><td>厂</td><td></td><td><button type="button" class="btn btn-huge btn-primary" onclick="play('han4');">han4</button></td><td>cliff</td></tr>
+		<tr><td>衣</td><td>衤</td><td><button type="button" class="btn btn-huge btn-danger" onclick="play('yi1');">yi1</button></td><td>clothes</td></tr>
+		<tr><td>色</td><td></td><td><button type="button" class="btn btn-huge btn-primary" onclick="play('se4');">se4</button></td><td>color</td></tr>
+		<tr><td>比</td><td></td><td><button type="button" class="btn btn-huge btn-success" onclick="play('bi3');">bi3</button></td><td>compare</td></tr>
+		<tr><td>尸</td><td></td><td><button type="button" class="btn btn-huge btn-danger" onclick="play('shi1');">shi1</button></td><td>corpse</td></tr>
+		<tr><td>牛</td><td>牜</td><td><button type="button" class="btn btn-huge btn-warning" onclick="play('niu2');">niu2</button></td><td>cow</td></tr>
+	</table>
+	</div>
+</div>
+<script src="js/chinese.js"></script>
+<script>$("#breadcrumb").html("&#174;&nbsp;radicals&nbsp;激进 > ENGLISH: A - C >");</script>
+</body>
+</html>
