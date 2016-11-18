f7033731b850d09851e233468ba03d3335455f79
Stephen McCready <stephenmccready@gmail.com>
2016-11-18 21:19:41 +0800

    Create fruit.php

---
+++ fruit.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" content=""/>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>&#127820; fruit 水果</title>
+	<link type="text/css" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<!--[if IE]>
+	<script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<link rel="stylesheet" href="css/chinese.css">
+</head>
+<body>
+<audio controls="" id="tao2"><source src="audio/tao2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="gua1"><source src="audio/gua1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="ying1"><source src="audio/ying1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="xi1"><source src="audio/xi1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="huo3"><source src="audio/huo3.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="long2"><source src="audio/long2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="guo3"><source src="audio/guo3.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="bo1"><source src="audio/bo1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="guo2"><source src="audio/guo2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="luo2"><source src="audio/luo2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="ping2"><source src="audio/ping2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="xian1"><source src="audio/xian1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="jiao1"><source src="audio/jiao1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="you4"><source src="audio/you4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="mang2"><source src="audio/mang2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="ju2"><source src="audio/ju2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="zi1"><source src="audio/zi1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="fan1"><source src="audio/fan1.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="mu4"><source src="audio/mu4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="li2"><source src="audio/li2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="lan2"><source src="audio/lan2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="mei2"><source src="audio/mei2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="pu2"><source src="audio/pu2.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<audio controls="" id="xing4"><source src="audio/xing4.mp3" preload="metadata" type="audio/mpeg">No audio</audio>
+<?php include 'navbar.html'; ?>
+<div class="container-fluid">
+	<div class="box">
+		<div>apple</div><div class="character">苹果</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('ping2');">píng</button>
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('guo2');">guó</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>apricot</div><div class="character">杏</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-primary" onclick="play('xing4');">xìng</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>banana</div><div class="character">香蕉</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('xian1');">xiāng</button>
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('jiao1');">jiāo</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>blue berry</div><div class="character">蓝莓</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('lan2');">lán</button>
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('mei2');">méi</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>cherry</div><div class="character">樱桃</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('ying1');">yīng</button>
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('tao2');">táo</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>dragon fruit</div><div class="character">火龙果</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-success" onclick="play('huo3');">huǒ</button>
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('long2');">lóng</button>
+			<button type="button" class="btn btn-huge btn-success" onclick="play('guo3');">guǒ</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>grape</div><div class="character">葡萄</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('pu2');">pú</button>
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('tao2');">táo</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>mango</div><div class="character">芒果</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('mang2');">máng</button>
+			<button type="button" class="btn btn-huge btn-success" onclick="play('guo3');">guǒ</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>melon</div><div class="character">瓜</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('gua1');">guā</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>orange</div><div class="character">橘子</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('ju2');">jú</button>
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('zi1');">zi</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>papaya</div><div class="character">番木瓜</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('fan1');">fān</button>
+			<button type="button" class="btn btn-huge btn-primary" onclick="play('mu4');">mù</button>
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('gua1');">guā</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>peach</div><div class="character">桃子</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('tao2');">táo</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>pear</div><div class="character">梨</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('li2');">lí</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>pineapple</div><div class="character">菠萝</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('bo1');">bō</button>
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('luo2');">luó</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>pomelo</div><div class="character">柚</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-primary" onclick="play('you4');">yòu</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>watermelon</div><div class="character">西瓜</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('xi1');">xī</button>
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('gua1');">guā</button>
+		</div>
+	</div>
+	<div class="box">
+		<div>yuzu</div><div class="character">柚子</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-primary" onclick="play('you4');">yòu</button>
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('zi1');">zi1</button>
+		</div>
+	</div>
+</div>
+<script src="js/chinese.js"></script>
+<script>$("#breadcrumb").html("&#127835;&nbsp;food&nbsp;餐饮 > &#127820;&nbsp;fruit&nbsp;水果 >");</script>
+</body>
+</html>
