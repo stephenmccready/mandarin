c2a760e23723124d575f081d27cae39e8e344397
Stephen McCready <stephenmccready@gmail.com>
2016-12-20 07:37:38 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Chinese 中文</title>
+	<title>Mandarin 普通话</title>
 	<link type="text/css" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
