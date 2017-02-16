95d8032a776f090c8e2d4a0b482ac5dc7910d012
Stephen McCready <stephenmccready@gmail.com>
2017-02-16 21:39:11 +0800

    Update numbers.php

--- numbers.php
+++ numbers.php
@@ -13,6 +13,8 @@
 	<link rel="stylesheet" href="css/chinese.css">
 </head>
 <body>
+<audio controls="" id="ji1"><source src="audio/ji1.mp3" type="audio/mpeg">No audio</audio>
+<audio controls="" id="ben3"><source src="audio/ben3.mp3" type="audio/mpeg">No audio</audio>
 <audio controls="" id="ling2"><source src="audio/ling2.mp3" type="audio/mpeg">No audio</audio>
 <audio controls="" id="yi1"><source src="audio/yi1.mp3" type="audio/mpeg">No audio</audio>
 <audio controls="" id="er4"><source src="audio/er4.mp3" type="audio/mpeg">No audio</audio>
@@ -29,59 +31,61 @@
 <audio controls="" id="wan4"><source src="audio/wan4.mp3" type="audio/mpeg">No audio</audio>
 <audio controls="" id="yi2"><source src="audio/yi2.mp3" type="audio/mpeg">No audio</audio>
 <audio controls="" id="yi4"><source src="audio/yi4.mp3" type="audio/mpeg">No audio</audio>
+<audio controls="" id="shu4"><source src="audio/shu4.mp3" type="audio/mpeg">No audio</audio>
+<audio controls="" id="zi1"><source src="audio/zi1.mp3" type="audio/mpeg">No audio</audio>
 <?php include 'navbar.html'; ?>
 <div class="container-fluid">
 	<div class="box">
-		<div>0</div><div class="character">零</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-warning" onclick="play('ling2');">líng</button></div>
+		<div>0</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-warning" onclick="play('ling2');"><div class="character">零</div>líng</button></div>
 	</div>
 	<div class="box">
-		<div>1</div><div class="character">一</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-danger" onclick="play('yi1');">yī</button></div>
+		<div>1</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-danger" onclick="play('yi1');"><div class="character">一</div>yī</button></div>
 	</div>
 	<div class="box">
-		<div>2</div><div class="character">二</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-primary" onclick="play('er4');">èr</button></div>
+		<div>2</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-primary" onclick="play('er4');"><div class="character">二</div>èr</button></div>
 	</div>
 	<div class="box">
-		<div>3</div><div class="character">三</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-danger" onclick="play('san1');">sān</button></div>
+		<div>3</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-danger" onclick="play('san1');"><div class="character">三</div>sān</button></div>
 	</div>
 	<div class="box">
-		<div>4</div><div class="character">四</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-primary" onclick="play('si4');">sì</button></div>
+		<div>4</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-primary" onclick="play('si4');"><div class="character">四</div>sì</button></div>
 	</div>
 	<div class="box">
-		<div>5</div><div class="character">五</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-success" onclick="play('wu3');">wǔ</button></div>
+		<div>5</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-success" onclick="play('wu3');"><div class="character">五</div>wǔ</button></div>
 	</div>
 	<div class="box">
-		<div>6</div><div class="character">六</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-primary" onclick="play('liu4');">liù</button></div>
+		<div>6</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-primary" onclick="play('liu4');"><div class="character">六</div>liù</button></div>
 	</div>
 	<div class="box">
-		<div>7</div><div class="character">七</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-danger" onclick="play('qi1');">qī</button></div>
+		<div>7</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-danger" onclick="play('qi1');"><div class="character">七</div>qī</button></div>
 	</div>
 	<div class="box">
-		<div>8</div><div class="character">八</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-danger" onclick="play('ba1');">bā</button></div>
+		<div>8</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-danger" onclick="play('ba1');"><div class="character">八</div>bā</button></div>
 	</div>
 	<div class="box">
-		<div>9</div><div class="character">九</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-success" onclick="play('jiu3');">jiǔ</button></div>
+		<div>9</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-success" onclick="play('jiu3');"><div class="character">九</div>jiǔ</button></div>
 	</div>
 	<div class="box">
-		<div>10</div><div class="character">十</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-warning" onclick="play('shi2');">shí</button></div>
+		<div>10</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-warning" onclick="play('shi2');"><div class="character">十</div>shí</button></div>
 	</div>
 	<div class="box">
-		<div>11</div><div class="character">十一</div><div class="btn-group btn-group-lg">
-			<button type="button" class="btn btn-huge btn-warning" onclick="play('shi2');">shí</button>
-			<button type="button" class="btn btn-huge btn-danger" onclick="play('yi1');">yī</button>
+		<div>11</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('shi2');"><div class="character">十</div>shí</button>
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('yi1');"><div class="character">一</div>yī</button>
 		</div>
 	</div>
 	<div class="box">
-		<div>21</div><div class="character">二十一</div><div class="btn-group btn-group-lg">
-			<button type="button" class="btn btn-huge btn-primary" onclick="play('er4');">èr</button>
-			<button type="button" class="btn btn-huge btn-warning" onclick="play('shi2');">shí</button>
-			<button type="button" class="btn btn-huge btn-danger" onclick="play('yi1');">yī</button>
+		<div>21</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-primary" onclick="play('er4');"><div class="character">二</div>èr</button>
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('shi2');"><div class="character">十</div>shí</button>
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('yi1');"><div class="character">一</div>yī</button>
 		</div>
 	</div>
 	<div class="box">
-		<div>100</div><div class="character">百</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-success" onclick="play('bai3');">bǎi</button></div>
+		<div>100</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-success" onclick="play('bai3');"><div class="character">百</div>bǎi</button></div>
 	</div>
 	<div class="box">
-		<div>1000</div><div class="character">千</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-danger" onclick="play('qian1');">qiān</button></div>
+		<div>1000</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-danger" onclick="play('qian1');"><div class="character">千</div>qiān</button></div>
 	</div>
 	<div class="box">
 		<div>1,0000<br />ten thousand</div><div class="character">万</div><div class="btn-group btn-group-lg">
@@ -104,6 +108,6 @@
 	</div>
 </div>
 <script src="js/chinese.js"></script>
-<script>$("#breadcrumb").html("&#9986;&nbsp;basics&nbsp;基本 > &nbsp;&#128290;&nbsp;numbers&nbsp;数 >");</script>
+<script>$("#breadcrumb").html("&#9986;&nbsp;basics&nbsp;<div class='btn-group'><button type='button' class='btn btn-nav btn-danger' onclick='play(&#39;ji1&#39;);'>基 jī</button><button type='button' class='btn btn-nav btn-success' onclick='play(&#39;ben3&#39;);'>本 běn</button>&nbsp;&#9658&nbsp;</div>"+"&nbsp;&#128290;&nbsp;numbers&nbsp;<div class='btn-group'><button type='button' class='btn btn-nav btn-primary' onclick='play(&#39;shu4&#39;);'>数 shù</button><button type='button' class='btn btn-nav btn-primary' onclick='play(&#39;zi1&#39;);'>字 zì</button></div>");</script>
 </body>
 </html>
