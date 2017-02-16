3a4bf27f207c966050300ca34301c7a4bca76724
Stephen McCready <stephenmccready@gmail.com>
2017-02-16 21:20:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -17,14 +17,17 @@
 <audio controls="" id="ma2"><source src="audio/ma2.mp3" type="audio/mpeg">No audio</audio>
 <audio controls="" id="ma3"><source src="audio/ma3.mp3" type="audio/mpeg">No audio</audio>
 <audio controls="" id="ma4"><source src="audio/ma4.mp3" type="audio/mpeg">No audio</audio>
+<audio controls="" id="jia1"><source src="audio/jia1.mp3" type="audio/mpeg">No audio</audio>
 <?php include 'navbar.html'; ?>
 <div class="container-fluid">
-	<div class="box"><div>mother</div><div class="character">媽</div><div><button type="button" class="btn btn-huge btn-danger" onclick="play('ma1');">mā 1</button></div></div>
-	<div class="box"><div>hemp</div><div class="character">麻</div><div><button type="button" class="btn btn-huge btn-warning" onclick="play('ma2');">má 2</button></div></div>
-	<div class="box"><div>horse</div><div class="character">馬</div><div><button type="button" class="btn btn-huge btn-success" onclick="play('ma3');">mǎ 3</button></div></div>
-	<div class="box"><div>scold</div><div class="character">罵</div><div><button type="button" class="btn btn-huge btn-primary" onclick="play('ma4');">mà 4</button></div></div>
+	<div>The 4 tones of Mandarin</div>
+	<div class="clearfix"></div>
+	<div class="box"><div>mother</div><div><button type="button" class="btn btn-huge btn-danger" onclick="play('ma1');"><div class="character">媽</div>shī 1</button></div></div>
+	<div class="box"><div>hemp</div><div><button type="button" class="btn btn-huge btn-warning" onclick="play('ma2');"><div class="character">麻</div>má 2</button></div></div>
+	<div class="box"><div>horse</div><div><button type="button" class="btn btn-huge btn-success" onclick="play('ma3');"><div class="character">馬</div>mǎ 3</button></div></div>
+	<div class="box"><div>scold</div><div><button type="button" class="btn btn-huge btn-primary" onclick="play('ma4');"><div class="character">罵</div>mà 4</button></div></div>
 </div>
 <script src="js/chinese.js"></script>
-<script>$("#breadcrumb").html("&#127968;&nbsp;home&nbsp;家");</script>
+<script>$("#breadcrumb").html("&#127968;&nbsp;home&nbsp;<button type='button' class='btn btn-nav btn-danger' onclick='play(&#39;jia1&#39;);'>家 jiā</button>");</script>
 </body>
 </html>
