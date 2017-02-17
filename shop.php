9b044829fa97c4218e176cf6f8d7682d7fc86827
Stephen McCready <stephenmccready@gmail.com>
2017-02-17 11:47:49 +0800

    Update shop.php

--- shop.php
+++ shop.php
@@ -13,6 +13,9 @@
 	<link rel="stylesheet" href="css/chinese.css">
 </head>
 <body>
+<audio controls="" id="ji1"><source src="audio/ji1.mp3" type="audio/mpeg">No audio</audio>
+<audio controls="" id="ben3"><source src="audio/ben3.mp3" type="audio/mpeg">No audio</audio>
+<audio controls="" id="dian4"><source src="audio/dian4.mp3" type="audio/mpeg">No audio</audio>
 <audio controls="" id="yuan2"><source src="audio/yuan2.mp3" type="audio/mpeg">No audio</audio>
 <audio controls="" id="duo1"><source src="audio/duo1.mp3" type="audio/mpeg">No audio</audio>
 <audio controls="" id="shao3"><source src="audio/shao3.mp3" type="audio/mpeg">No audio</audio>
@@ -38,82 +41,82 @@
 <?php include 'navbar.html'; ?>
 <div class="container-fluid">
 	<div class="box">
-		<div>yuán</div><div class="character">元</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-warning" onclick="play('yuan2');">yuán</button></div>
+		<div>yuán</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-warning" onclick="play('yuan2');"><div class="character">元</div>yuán</button></div>
 	</div>
 	<div class="box">
-		<div>how much?</div><div class="character">若干</div><div class="btn-group btn-group-lg">
-			<button type="button" class="btn btn-huge btn-danger" onclick="play('duo1');">duō</button>
-			<button type="button" class="btn btn-huge btn-success" onclick="play('shao3');">shǎo</button>
+		<div>how much?</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('duo1');"><div class="character">若</div>duō</button>
+			<button type="button" class="btn btn-huge btn-success" onclick="play('shao3');"><div class="character">干</div>shǎo</button>
 		</div>
 	</div>
 	<div class="box">
-		<div>do you have?</div><div class="character">你有</div><div class="btn-group btn-group-lg">
-			<button type="button" class="btn btn-huge btn-success" onclick="play('ni3');">nǐ</button>
-			<button type="button" class="btn btn-huge btn-success" onclick="play('you3');">yǒu</button>
+		<div>do you have?</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-success" onclick="play('ni3');"><div class="character">你</div>nǐ</button>
+			<button type="button" class="btn btn-huge btn-success" onclick="play('you3');"><div class="character">有</div>yǒu</button>
 		</div>
 	</div>
 	<div class="box">
-		<div>just looking</div><div class="character">看看</div><div class="btn-group btn-group-lg">
-			<button type="button" class="btn btn-huge btn-primary" onclick="play('kan4');">kàn</button>
-			<button type="button" class="btn btn-huge btn-primary" onclick="play('kan4');">kàn</button>
-		</div>
-	</div>
-	<div class="clearfix"></div>
-	<div class="box">
-		<div>small</div><div class="character">小</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-success" onclick="play('xiao3');">xiǎo</button></div>
-	</div>
-	<div class="box">
-		<div>medium</div><div class="character">中</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-danger" onclick="play('zhong1');">zhōng</button></div>
-	</div>
-	<div class="box">
-		<div>large</div><div class="character">大</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-primary" onclick="play('da4');">dà</button></div>
-	</div>
-	<div class="box">
-		<div>extra large</div><div class="character">加大号</div><div class="btn-group btn-group-lg">
-			<button type="button" class="btn btn-huge btn-danger" onclick="play('jia1');">jiā</button>
-			<button type="button" class="btn btn-huge btn-primary" onclick="play('da4');">dà</button>
-			<button type="button" class="btn btn-huge btn-primary" onclick="play('hao4');">hào</button>
+		<div>just looking</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-primary" onclick="play('kan4');"><div class="character">看</div>kàn</button>
+			<button type="button" class="btn btn-huge btn-primary" onclick="play('kan4');"><div class="character">看</div>kàn</button>
 		</div>
 	</div>
 	<div class="clearfix"></div>
 	<div class="box">
-		<div>tie</div><div class="character">领带</div><div class="btn-group btn-group-lg">
-			<button type="button" class="btn btn-huge btn-success" onclick="play('ling3');">lǐng</button>
-			<button type="button" class="btn btn-huge btn-primary" onclick="play('dai4');">dài</button>
+		<div>small</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-success" onclick="play('xiao3');"><div class="character">小</div>xiǎo</button></div>
+	</div>
+	<div class="box">
+		<div>medium</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-danger" onclick="play('zhong1');"><div class="character">中</div>zhōng</button></div>
+	</div>
+	<div class="box">
+		<div>large</div><div class="btn-group btn-group-lg"><button type="button" class="btn btn-huge btn-primary" onclick="play('da4');"><div class="character">大</div>dà</button></div>
+	</div>
+	<div class="box">
+		<div>extra large</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('jia1');"><div class="character">加</div>jiā</button>
+			<button type="button" class="btn btn-huge btn-primary" onclick="play('da4');"><div class="character">大</div>dà</button>
+			<button type="button" class="btn btn-huge btn-primary" onclick="play('hao4');"><div class="character">号</div>hào</button>
+		</div>
+	</div>
+	<div class="clearfix"></div>
+	<div class="box">
+		<div>tie</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-success" onclick="play('ling3');"><div class="character">领</div>lǐng</button>
+			<button type="button" class="btn btn-huge btn-primary" onclick="play('dai4');"><div class="character">带</div>dài</button>
 		</div>
 	</div>
 	<div class="box">
-		<div>suit</div><div class="character">西服</div><div class="btn-group btn-group-lg">
-			<button type="button" class="btn btn-huge btn-danger" onclick="play('xi1');">xī</button>
-			<button type="button" class="btn btn-huge btn-warning" onclick="play('fu2');">fú</button>
+		<div>suit</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('xi1');"><div class="character">西</div>xī</button>
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('fu2');"><div class="character">服</div>fú</button>
 		</div>
 	</div>
 	<div class="box">
-		<div>coat</div><div class="character">外套</div><div class="btn-group btn-group-lg">
-			<button type="button" class="btn btn-huge btn-primary" onclick="play('wai4');">wài</button>
-			<button type="button" class="btn btn-huge btn-primary" onclick="play('tao4');">tào</button>
+		<div>coat</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-primary" onclick="play('wai4');"><div class="character">外</div>wài</button>
+			<button type="button" class="btn btn-huge btn-primary" onclick="play('tao4');"><div class="character">套</div>tào</button>
 		</div>
 	</div>
 	<div class="box">
-		<div>jacket</div><div class="character">夹克</div><div class="btn-group btn-group-lg">
-			<button type="button" class="btn btn-huge btn-warning" onclick="play('jia2');">jiá</button>
-			<button type="button" class="btn btn-huge btn-primary" onclick="play('ke4');">kè</button>
+		<div>jacket</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('jia2');"><div class="character">夹</div>jiá</button>
+			<button type="button" class="btn btn-huge btn-primary" onclick="play('ke4');"><div class="character">克</div>kè</button>
 		</div>
 	</div>
 	<div class="box">
-		<div>skirt</div><div class="character">裙子</div><div class="btn-group btn-group-lg">
-			<button type="button" class="btn btn-huge btn-warning" onclick="play('qun2');">qún</button>
-			<button type="button" class="btn btn-huge btn-danger" onclick="play('zi1');">zi</button>
+		<div>skirt</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-warning" onclick="play('qun2');"><div class="character">裙</div>qún</button>
+			<button type="button" class="btn btn-huge btn-danger" onclick="play('zi1');"><div class="character">子</div>zi</button>
 		</div>
 	</div>
 	<div class="box">
-		<div>gloves</div><div class="character">手套</div><div class="btn-group btn-group-lg">
-			<button type="button" class="btn btn-huge btn-success" onclick="play('shou3');">shǒu</button>
-			<button type="button" class="btn btn-huge btn-primary" onclick="play('tao4');">tào</button>
+		<div>gloves</div><div class="btn-group btn-group-lg">
+			<button type="button" class="btn btn-huge btn-success" onclick="play('shou3');"><div class="character">手</div>shǒu</button>
+			<button type="button" class="btn btn-huge btn-primary" onclick="play('tao4');"><div class="character">套</div>tào</button>
 		</div>
 	</div>
 </div>
 <script src="js/chinese.js"></script>
-<script>$("#breadcrumb").html("&#9986;&nbsp;basics&nbsp;基本 > &#128179;&nbsp;shopping&nbsp;店 >");</script>
+<script>$("#breadcrumb").html("&#9986;&nbsp;basics&nbsp;<div class='btn-group'><button type='button' class='btn btn-nav btn-danger' onclick='play(&#39;ji1&#39;);'>基 jī</button><button type='button' class='btn btn-nav btn-success' onclick='play(&#39;ben3&#39;);'>本 běn</button>&nbsp;&#9658&nbsp;</div> &#128179;&nbsp;shopping&nbsp;<button type='button' class='btn btn-nav btn-primary' onclick='play(&#39;dian4&#39;);'>店 diàn</button>");</script>
 </body>
 </html>
